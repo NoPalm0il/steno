ce5f6676f3e675a0ae3d00920302eab9e7a9a8d7
NoPalm0il <jpedrocr2000@hotmail.com>
2021-11-24 16:15:22 +0000

    added user input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use std::str::FromStr;
+use std::{env, io, process};
 
 mod cryptor;
 
 fn main() {
-    let file_name = String::from_str("image.png").unwrap();
-    let cry = cryptor::Cryptor::new(file_name);
-    cry.write_message(String::new());
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        print_help_and_exit();
+    }
+
+    let fileop = &args[1];
+    let filename = &args[2];
+
+    let mut cry = cryptor::Cryptor::new(filename.to_string());
+    match &fileop[..] {
+        "r" => cry.read_message(),
+        "w" => {
+            println!("Write your message, press Enter to finish.\n");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Incorrect string input!");
+            cry.write_message(input);
+        }
+        _ => print_help_and_exit(),
+    }
+}
+
+fn print_help_and_exit() {
+    let help = "usage: steno <r|w> <image.png>";
+    println!("{}", help);
+    process::exit(0);
 }
